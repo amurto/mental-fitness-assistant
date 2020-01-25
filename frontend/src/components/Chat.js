ed8a0079721c1aaac08b63ad9b5c482029dcc2ec
amurto <amurtobasu@gmail.com>
2020-01-25 16:42:55 +0530

    changes

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -8,18 +8,18 @@ export default function Chat() {
                 <div className="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
                 <div className="media-body ml-3">
                     <div className="bg-light rounded py-2 px-3 mb-2">
-                    <p className="text-small mb-0 text-muted">Test which is a new approach all solutions</p>
+                    <p className="text-small mb-0 text-muted">Hello</p>
                     </div>
-                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                    <p className="small text-muted">3:00 PM | Jan 25</p>
                 </div>
                 </div>
 
                 <div className="media w-50 ml-auto mb-3">
                 <div className="media-body">
                     <div className="bg-primary rounded py-2 px-3 mb-2">
-                    <p className="text-small mb-0 text-white">Test which is a new approach to have all solutions</p>
+                    <p className="text-small mb-0 text-white">Hello Sir! I want an appointment with you tomorrow. Would be give me a timing please.</p>
                     </div>
-                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                    <p className="small text-muted">3:02 PM | Jan 25</p>
                 </div>
                 </div>
 
@@ -27,9 +27,9 @@ export default function Chat() {
                 <div className="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
                 <div className="media-body ml-3">
                     <div className="bg-light rounded py-2 px-3 mb-2">
-                    <p className="text-small mb-0 text-muted">Test, which is a new approach to have</p>
+                    <p className="text-small mb-0 text-muted">Ok, but where do you live and what is the cause of your appointment? </p>
                     </div>
-                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                    <p className="small text-muted">3:03 PM | Jan 25</p>
                 </div>
                 </div>
 
@@ -37,27 +37,27 @@ export default function Chat() {
                 <div className="media w-50 ml-auto mb-3">
                 <div className="media-body">
                     <div className="bg-primary rounded py-2 px-3 mb-2">
-                    <p className="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                    <p className="text-small mb-0 text-white">Sir, I have been having suicidal thoughts lately.</p>
                     </div>
-                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                    <p className="small text-muted">3:04 PM | Jan 25</p>
                 </div>
                 </div>
 
                 <div className="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
                 <div className="media-body ml-3">
                     <div className="bg-light rounded py-2 px-3 mb-2">
-                    <p className="text-small mb-0 text-muted">Test, which is a new approach</p>
+                    <p className="text-small mb-0 text-muted">Damn please! Don't take such a foolish step.</p>
                     </div>
-                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                    <p className="small text-muted">3:05 PM | Jan 25</p>
                 </div>
                 </div>
 
                 <div className="media w-50 ml-auto mb-3">
                 <div className="media-body">
                     <div className="bg-primary rounded py-2 px-3 mb-2">
-                    <p className="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                    <p className="text-small mb-0 text-white">Thank you sir for the kind words but Happy bot already stopped me from committing that crime.</p>
                     </div>
-                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                    <p className="small text-muted">3:05 PM | Jan 25</p>
                 </div>
                 </div>
 
